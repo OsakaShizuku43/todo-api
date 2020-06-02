25d0fb23250b903bfb384f2c8b733d10c051cc11
Zimin Cao <caozmbill@gmail.com>
2020-06-02 13:02:54 -0400

    Address security issues on dependencies

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const app = require('express')();
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const http = require('http');
 const db = require('./db');
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
